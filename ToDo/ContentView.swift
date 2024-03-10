0614bd4d3b61143b5b48534c142b18ab1e876cb5
Nitin <singhmanhasnitin@gmail.com>
2024-03-10 12:46:55 +0530

    revert

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -29,7 +29,6 @@ struct ContentView: View {
 
     private var content: some View {
         VStack {
-            Text("helo")
             ForEach(textFields.indices, id: \.self) { index in
                 HStack {
                     TextField("Password", text: $textFields[index])
